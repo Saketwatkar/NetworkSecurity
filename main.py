3071d298eba93262f2fb5c90140631d01b0e3d45
Saket Watkar <saketwatkar@gmail.com>
2025-08-07 11:21:00 +0530

    Training and evaluation of mlflow commited

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from network_security.components.data_validation import DataValidation
 from network_security.components.data_transformation import DataTransformation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
-from network_security.entity.config_entity import DataValidationConfig,DataTransformationConfig
 
-
-# from networksecurity.components.model_trainer import ModelTrainer
-# from networksecurity.entity.config_entity import ModelTrainerConfig
+from network_security.components.model_trainer import ModelTrainer
+from network_security.entity.config_entity import ModelTrainerConfig
  
 
 import sys
@@ -36,37 +34,13 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
 
-        # logging.info("Model Training artifact created")
+        logging.info("Model Training artifact created")
         
-        
-        
-    except Exception as e:
-           raise NetworkSecurityException(e,sys)
-
-
-import sys
-
-if __name__=='__main__':
-    try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("Initiate the data ingestion")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
-        data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        data_validation=DataValidation(dataingestionartifact,data_validation_config)
-        logging.info("Initiate the data Validation")
-        data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("data Validation Completed")
-        print(data_validation_artifact)
-       
         
         
     except Exception as e:
